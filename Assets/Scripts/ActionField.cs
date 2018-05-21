4b532dec0bdf6589b2500efcf2871c6cc59f072a
April <admdizonn@gmail.com>
2018-05-20 22:44:24 -0400

    updates to the action field
    action field can generate the ground sprites within range. however, the range is incorrect

--- Assets/Scripts/ActionField.cs
+++ Assets/Scripts/ActionField.cs
@@ -18,6 +18,7 @@ public class ActionField : MonoBehaviour {
     private float fieldWidth;
     private float fieldX; //the x coordinate of the top left corner of the action field
     private float fieldY; //the y coordinate of the top left corner of the action field
+    private Vector3 field; //Vector3 coordinates of the top left corner of the action field
 
     int numGroundSprites; //current number of ground sprites
     int maxGroundSprites; //max number of ground sprites
@@ -42,29 +43,30 @@ public class ActionField : MonoBehaviour {
         maxGroundSpritesX = (int)(fieldWidth / 32f) * 2;
         maxGroundSpritesY = (int)(fieldHeight / 32f) * 2;
 
-        Debug.Log("maxGroundSpritesX: " + maxGroundSpritesX);
-        Debug.Log("maxGroundSpritesY: " + maxGroundSpritesY);
-        Debug.Log("Fieldheight: " + fieldHeight);
-        Debug.Log("fieldwidth: " + fieldWidth);
-
         groundSprites = new GroundSprite[maxGroundSpritesY, maxGroundSpritesX];
         InitializeGroundSprites();
-
-
 
 	}
 	
 
 
+
 	// Update is called once per frame
-	void Update () {
+	void LateUpdate () {
         center = player.position; //true center of the field
 
         //Update the X and Y coordinates of the action field
-        fieldX = center.x - (fieldWidth / 2);
-        fieldY = center.y + (fieldHeight / 2);
+        fieldX = center.x - ((fieldWidth)) / 100 * -1;
+        fieldY = center.y + ((fieldHeight)) / 100;
+        field = new Vector3(fieldX, fieldY, 0);
+
+        Debug.Log("field: " + field);
+
+        //Update the ground sprites within the action field
+        UpdateGroundSprites();
 
     }
+
 
 
 
@@ -76,21 +78,108 @@ public class ActionField : MonoBehaviour {
 
 
  
-    /* Spawn ground sprite game objects, within the action field and destroy 
-     * ground sprite game objects outside of the field.
+    /* Destroy grounf sprites that are outside the action field and place new
+     * ground sprites within the action field
      */
     private void UpdateGroundSprites()
     {
         //loop through the ground array to create ground sprites
-        for (int i = 0; i < maxGroundSpritesX; i++)
+        for (int i = 0; i < maxGroundSpritesY; i++)
         {
-            for (int j = 0; j < maxGroundSpritesY; j++)
+            for (int j = 0; j < maxGroundSpritesX; j++)
             {
                 //Check if this sprite is within the action field
+                if (WithinActionField(groundSprites[i,j].position))
+                {
+                    //Within the action field
+                    //Do nothing (may be changed later)
+                    //Debug.Log("witin field");
+                }
+                else
+                {
+                    //Not within the action field
+                    //Delete the ground sprite and reposition it within the field
+                    Vector3 newPosition = RepoGroundSprite(groundSprites[i, j].position);
+                    groundSprites[i, j].DestroyGroundSprite();
+                    groundSprites[i, j].NewGroundSprite(newPosition);
+                    Debug.Log("New ground field placed");
+                }
             }
         }
-        
     }
+
+
+
+
+    /* Check if a sprite is within the rectangular action fied by comparing the x and y coordinates
+     * Input: Vector3 position of a specific sprite
+     * Returns true if it is in the action field and false otherwise
+     */
+    private bool WithinActionField(Vector3 sprite)
+    {
+        //check if the x coordinate of the sprite is within (both sides) of the field
+        if (sprite.x < fieldX)
+        {
+            //sprite is beyond the left side of the field
+            return false;
+        }
+        else if (sprite.x > (center.x + ((fieldWidth)) / 100 * -1))
+        {
+            //sprite is beyond the right side of the field
+            return false;
+        }
+        else if (sprite.y > fieldY)
+        {
+            //sprite is beyond the top side of the field
+            return false;
+        }
+        else if (sprite.y < (center.y - ((fieldHeight)) / 100))
+        {
+            //sprite is beyond the bottom side of the field
+            return false;
+        }
+
+        //else, it is within range, return true
+        return true;
+    }
+
+
+
+
+    /* Given a ground sprite that is OUTSIDE the action field, put it within the action field
+     * Input: position of the ground sprite that is outside the action field
+     * Returns: new position of the ground sprite that is inside the action field
+     */ 
+    private Vector3 RepoGroundSprite(Vector3 sprite)
+    {
+        Vector3 newPosition = sprite;
+        //Find where the sprite needs to be repositioned
+        if (sprite.x < fieldX)
+        {
+            //sprite is beyond the left side of the field, can be put on the right side of the field
+            sprite.x = (center.x + ((fieldWidth)) / 100 * -1); 
+        }
+        if (sprite.x > (center.x + ((fieldWidth)) / 100 * -1))
+        {
+            //sprite is beyond the right side of the field, can be put on the left side of the field
+            sprite.x = fieldX;
+        }
+        if (sprite.y < (center.y - ((fieldHeight)) / 100))
+        {
+            //sprite is beyond the bottom side of the field, can be put on the top side of the field
+            sprite.y = fieldY;
+        }
+        if (sprite.y > fieldY)
+        {
+            //sprite is beyond the top side of the field, can be put on the bottom side of the field
+            sprite.y = (center.y - ((fieldHeight)) / 100);
+        }
+
+        return newPosition;
+    }
+
+
+
 
 
     /* Spawn ground sprite game objects within the action field at start
@@ -123,6 +212,8 @@ public class ActionField : MonoBehaviour {
             offsetY -= sizeOfSprite;
         }
     }
+
+
 
 
     /* Find the correct value for the height of the action field
